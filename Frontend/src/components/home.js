4358b47b707af3c0b4ed94d5a4e5ab9da2ffcf8e
Gppsv143 <97022674+Gppsv143@users.noreply.github.com>
2024-12-17 18:12:09 +0530

    Create home.js

---
+++ Frontend/src/components/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.png'; // Import the logo image
+
+const Home = () => {
+  return (
+    <div>
+      <img src={logo} alt="App Logo" />
+      <h1>Welcome to Praveen Travels</h1>
+    </div>
+  );
+};
+
+export default Home;
